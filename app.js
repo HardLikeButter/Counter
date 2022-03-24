8793b730439260d3fe42b1160892887c74aeba49
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-03-24 19:52:37 -0400

    Update Website

---
+++ app.js
@@ -0,0 +1,58 @@
+/*
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        }  else {
+            count = 0;
+        } 
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#FFF";
+        }
+        value.textContent = count;
+    });
+});
+*/
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("reset")) {
+            count = 0;
+        } if (styles.contains("increase")) {
+            count++;
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#FFF";
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        value.textContent = count;
+    });
+});
