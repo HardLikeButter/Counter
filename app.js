392c7aaea9cbef8b5bbb491b66957c613956b6f2
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-05-25 12:55:48 -0400

    Update

--- app.js
+++ app.js
@@ -2,28 +2,31 @@
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
+
 let count = 0;
 
-
-btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
-        const styles = e.currentTarget.classList;
-        if (styles.contains("decrease")) {
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let styles = e.target.classList;
+        if (styles.contains('decrease')) {
             count--;
-        } else if (styles.contains("reset")) {
+        }
+        if (styles.contains('reset')) {
             count = 0;
-        } if (styles.contains("increase")) {
+        }
+        if (styles.contains('increase')) {
             count++;
         }
         if (count < 0) {
-            value.style.color = "red";
+            value.style.color = 'red';
         }
         if (count === 0) {
-            value.style.color = "#FFF";
+            value.style.color = '#FFF';
         }
         if (count > 0) {
-            value.style.color = "green";
+            value.style.color = 'green';
         }
         value.textContent = count;
     });
 });
+
